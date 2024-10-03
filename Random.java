6333b8117ef1c40bdb3073fd7b70d42ca718c526
Jawadul Chowdhury <sxlvin19@gmail.com>
2024-10-02 19:36:02 -0500

    New commit

---
+++ Random.java
@@ -0,0 +1,15 @@
+public class Random {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Test line 1");
+        System.out.println("Test line 2");
+        System.out.println("Test line 3");
+        System.out.println("Test line 4");
+        System.out.println("Test line 5");
+
+
+    }
+
+}
